dc82fd09265b3f511b1002a474805e0c47ffb0d5
theYounger <kuyehara1@gmail.com>
2016-06-09 16:13:33 -1000

    vault.js: 1st + 2nd pass

--- vault.js
+++ vault.js
@@ -1,4 +1,24 @@
-'use strict';
 module.exports = function() {
-  
+
+var vaultCode = {};
+
+  var setValue = function(key, value) {
+    vaultCode[key] = value;
+  };
+
+  var getValue = function(key) {
+    if(vaultCode.key === undefined) {
+      return null;
+    } else {
+    return vaultCode[key];
+    }
+  };
+
+
+  return {
+    setValue: setValue,
+    getValue: getValue
+  };
 };
+
+vault.setValue("mypass", "devleague");
